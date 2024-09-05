3d02a1688b8f8baef3a4d26dd7ead26e7fd21b6b
M4yankkkk <m4yank.t@gmail.com>
2024-09-06 00:03:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -57,7 +58,7 @@ app.get('/results', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
