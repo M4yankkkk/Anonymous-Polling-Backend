d6865a41452155d3fa2f49cb1fd199e4621678ed
M4yankkkk <m4yank.t@gmail.com>
2024-09-06 00:15:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Replace with your MongoDB connection string
-const uri = 'mongodb+srv://rockyxxxyash69:SqdjLFaTnPjHuUFA@cluster0-shard-00-00.2qeam.mongodb.net:27017/mydatabase?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://rockyxxxyash69:SqdjLFaTnPjHuUFA@cluster0-shard-00-00.2qeam.mongodb.net/mydatabase?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const voteSchema = new mongoose.Schema({
