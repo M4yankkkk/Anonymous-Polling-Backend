11cd0e077a2ce44f17275600e26c53b820ac8224
M4yankkkk <m4yank.t@gmail.com>
2024-09-05 22:10:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors()); 
 
-mongoose.connect('mongodb+srv://rockyxxxyash69:SqdjLFaTnPjHuUFA@cluster0.2qeam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb+srv://rockyxxxyash69:SqdjLFaTnPjHuUFA@cluster0.2qeam.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
